c2705bb03ca82d0379474e72f9740bfa7b267ea5
Ilya <fillomaniya@mail.ru>
2022-08-23 20:15:11 +0300

    commie Exercise_6

---
+++ Exercise_6/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Введите число. Узнаем - Чётное ли оно");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number % 2 == 0)
+{
+    Console.WriteLine("yes");
+}
+else
+{
+    Console.WriteLine("no");
+}

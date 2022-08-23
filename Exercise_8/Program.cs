4997041c52fb71581948bad6c2e5a998bf08909a
Ilya <fillomaniya@mail.ru>
2022-08-23 21:08:46 +0300

    commit Exercise_8

---
+++ Exercise_8/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// Console.Write("Введите число: ");
+// int number1 = 1;
+// int number2 = Convert.ToInt32(Console.ReadLine());
+
+// for (int i = number1; i <= number2; i++)
+// {
+//     if (i % 2 == 0) 
+//     {
+//         Console.Write(i + " ");
+//     }
+// }
+
+Console.Write("Введите число: ");
+int number1 = 1;
+int number2 = Convert.ToInt32(Console.ReadLine());
+int i = number1;
+while (i <= number2)
+{
+    if (i % 2 == 0) 
+    {
+        Console.Write(i + " ");
+    }
+    i ++;
+}

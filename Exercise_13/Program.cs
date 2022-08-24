e5740ab98964e4716d6218567a1ef39570f8642f
Ilya <fillomaniya@mail.ru>
2022-08-24 22:37:48 +0300

    commit Exercise_13

---
+++ Exercise_13/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число: ");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num > 99)
+{
+    Console.WriteLine(num.ToString()[2]);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}

af4103ff4e32ec9bf5611de00bc15fc104a339d9
Ilya <fillomaniya@mail.ru>
2022-08-24 22:21:44 +0300

    commit Exercise_10

---
+++ Exercise_10/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите трёхзначное число: ");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num > 999 || num < 100)
+{
+    Console.WriteLine("Вы ввели не трёхзначное число");
+}
+else
+{
+    int num2 = num % 100;
+    int num3 = num2 / 10;
+    Console.WriteLine(num3);
+}

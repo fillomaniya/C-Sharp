64d716a28d7adad4e3b568397d9d6dc6c26f9ba8
Ilya <fillomaniya@mail.ru>
2022-08-31 20:25:58 +0300

    Add commit Exercise_19

---
+++ Exercise_19/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая принимает на вход 
+// пятизначное число и проверяет, является ли оно палиндромом
+
+// int num = new Random().Next(10000,100000);
+// Console.WriteLine(num);
+
+// string num2 = num.ToString();
+// if(num2[0] == num2[4] && num2[1] == num2[3])
+// {
+//     Console.WriteLine("Yes");
+// }
+// else
+// {
+//     Console.WriteLine("No");
+// }
+Console.WriteLine("Введите пятизначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num < 10000 || num > 100000)
+{
+    Console.WriteLine("Ошибка. Число не пятизначное");
+}
+else
+{
+    string num2 = num.ToString();
+    if(num2[0] == num2[4] && num2[1] == num2[3])
+    {
+        Console.WriteLine("Yes");
+    }
+    else
+    {
+        Console.WriteLine("No");
+    }
+}

a0b94b17ead9a17ee826debb0eb4eabfd5fbb772
Ilya <fillomaniya@mail.ru>
2022-09-02 12:12:14 +0300

    commit Exercise_27

---
+++ Exercise_27/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int SumNumber(int num)
+{ 
+int count = Convert.ToString(num).Length;
+int advance = 0;
+int result = 0;
+
+for (int i = 0; i < count; i++)
+    {
+        advance = num - num % 10;
+        result = result + (num - advance);
+        num = num / 10;
+    }
+    return result;
+}
+
+int sumNumber = SumNumber(num);
+Console.WriteLine("Сумма цифр в числе: " + sumNumber);

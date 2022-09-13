38193e39183fb0acb8156167ec8a621900a694a8
Ilya <fillomaniya@mail.ru>
2022-09-13 20:26:56 +0300

    new commit Exercise_41

--- Exercise_41/Program.cs
+++ Exercise_41/Program.cs
@@ -1,4 +1,7 @@
-﻿Console.Write($"Введите количество чисел: ");
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write($"Введите количество чисел: ");
 int m = Convert.ToInt32(Console.ReadLine());
 int[] array = new int[m];
 
@@ -6,7 +9,7 @@ void FillArray(int m)
 {
     for (int i = 0; i < m; i++)
     {
-        Console.Write($"Введи {i + 1} число: ");
+        Console.Write($"Введите {i + 1} число: ");
         array[i] = Convert.ToInt32(Console.ReadLine());
     }
 }

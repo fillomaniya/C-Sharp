5bcbeadd80909b9559cacec8a3a9e5ac7d32d831
Ilya <fillomaniya@mail.ru>
2022-09-13 21:25:32 +0300

    2 new commit Exercise_43

--- Exercise_43/Program.cs
+++ Exercise_43/Program.cs
@@ -4,46 +4,31 @@
 // значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
-double[,] value = new double[2, 2];
-double[] crossPoint = new double[2];
+// Найти x и y: 
+// x = (b2 - b1) / (k1 - k2)
+// y = k1 * x + b1
 
-void Fill()
+Console.Write("Введите k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (b2 - b1) / (k1 - k2);
+double y = x * k1 + b1;
+
+if (k1 == k2 && b1 == b2)
 {
-    for (int i = 0; i < value.GetLength(0); i++)
-    {
-        Console.Write($"Введите числа {i + 1}-го уравнения (y = k * x + b):\n");
-        for (int j = 0; j < value.GetLength(1); j++)
-        {
-            if (j == 0) Console.Write($"Введите коэффициент k: ");
-            else Console.Write($"Введите коэффициент b: ");
-            value[i, j] = Convert.ToInt32(Console.ReadLine());
-        }
-    }
+    Console.Write("Прямые совпадают");
 }
-
-double[] Decision(double[,] value)
+else if (k1 == k2 && b1 != b2)
 {
-    crossPoint[0] = (value[1, 1] - value[0, 1]) / (value[0, 0] - value[1, 0]);
-    crossPoint[1] = crossPoint[0] * value[0, 0] + value[0, 1];
-    return crossPoint;
+    Console.Write("Прямые параллельны");
 }
-
-void result(double[,] value)
+else
 {
-    if (value[0, 0] == value[1, 0] && value[0, 1] == value[1, 1])
-    {
-        Console.Write($"\nПрямые совпадают");
-    }
-    else if (value[0, 0] == value[1, 0] && value[0, 1] != value[1, 1])
-    {
-        Console.Write($"\nПрямые параллельны");
-    }
-    else
-    {
-        Decision(value);
-        Console.Write($"\nТочка пересечения прямых: [{crossPoint[0]}, {crossPoint[1]}]");
-    }
+    Console.Write($"Точка пересечения прямых: [{x}, {y}]");
 }
-
-Fill();
-result(value);
